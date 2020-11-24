68533b5e483c109a4678561f1e13b91af56c5e95
jeison ramos <jeisonramos170@gmail.com>
2020-11-24 16:36:26 -0500

    completo mapeo

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -1,13 +1,16 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface ICategory extends Document {
+    iduser: string;
     name: string;
     description: string;
+    
    
 
 }
 
 const userSchema: Schema = new Schema({
+    iduser: { type: String, required: true },
     name: { type: String, required: true },
     description: { type: String, required: true },
    
--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,20 +1,24 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IProduct extends Document {
+    idproduct: number;
     name: string;
     type: string;
     price: number;
     description: string;
     disstock: number;
+    idcategory: number;
 
 }
 
 const userSchema: Schema = new Schema({
+    idproduct: { type: Number, required: true },
     name: { type: String, required: true },
     type: { type: String, required: true },
     price: { type: Number, required: true },
     description: { type: String, required: true },
-    disstock: { type: Number, required: true }
+    disstock: { type: Number, required: true },
+    idcategory: { type: Number, required: true }
 });
 
 export default mongoose.model<IProduct>('Product', userSchema);
